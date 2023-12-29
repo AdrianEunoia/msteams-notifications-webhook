229b7af484fb8ff43056b0ed8efe45c388be12e5
AdrianEunoia <adriangarciainbox@gmail.com>
2023-12-30 00:44:41 +0100

    :)

--- src/failure-script.sh
+++ src/failure-script.sh
@@ -4,4 +4,4 @@ cat << EOF > message.json
 {"@type":"MessageCard","@context":"https://schema.org/extensions","summary":"Pipeline failure!","themeColor":"ff0000","title":"CI $GITHUB_REPOSITORY pipeline failure ❌","sections":[{"facts":[{"name":"Repository:","value":"$GITHUB_REPOSITORY"},{"name":"Branch:","value":"$GITHUB_REF_NAME"},{"name":"By:","value":"$GITHUB_ACTOR"}]}],"potentialAction":[{"@type":"OpenUri","name":"View on GitHub","targets":[{"os":"default","uri":"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"}]}]}
 EOF
 
-curl -X POST ${{ secrets.WEBHOOK_TEAMS }} -H 'Content-Type: application/json' -d @message.json
+curl -X POST "${{ secrets.WEBHOOK_TEAMS }}" -H 'Content-Type: application/json' -d @message.json

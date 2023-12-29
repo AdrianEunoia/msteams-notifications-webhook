aff4c6458eb8abc6baf56f7da0554d8932bde5c0
AdrianEunoia <adriangarciainbox@gmail.com>
2023-12-30 00:52:57 +0100

    :)

--- src/success-script.sh
+++ src/success-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "WEBHOOK_TEAMS: $WEBHOOK_TEAMS"
+
 cat << EOF > message.json
 {"@type":"MessageCard","@context":"https://schema.org/extensions","summary":"Pipeline success!","themeColor":"008000","title":"CI $GITHUB_REPOSITORY pipeline success ","sections":[{"facts":[{"name":"Repository:","value":"$GITHUB_REPOSITORY"},{"name":"Branch:","value":"$GITHUB_REF_NAME"},{"name":"By:","value":"$GITHUB_ACTOR"}]}],"potentialAction":[{"@type":"OpenUri","name":"View on GitHub","targets":[{"os":"default","uri":"$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"}]}]}
 EOF
 
-curl -X POST "$WEBHOOK_TEAMS" -H 'Content-Type: application/json' -d @message.json
+curl -X POST -H 'Content-Type: application/json' -d @message.json "$WEBHOOK_TEAMS"
